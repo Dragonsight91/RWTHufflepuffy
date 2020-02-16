de49151a4ebf3eb99d1c3e9b2760a09b67a1c33e
Dragonsight91 <r.vanemelen@icloud.com>
2020-02-16 12:39:12 +0100

    add response to welcome command

--- src/commands/welcome.py
+++ src/commands/welcome.py
@@ -11,15 +11,13 @@ async def welcome_handler(bot, message):
         if len(message.role_mentions) == 0 or len(command) < 3:
             response = "Oops, something went wrong. Please mention the Role and your Name.\nThe command can be used like this: `$welcome {role} {name}`"
         else:
-            role = message.role_mentions
-            await member.add_roles(role[0])
-            
-            for x in command:
-                print(x)
+            role = message.role_mentions[0]
             nick = command[2]
-            await member.edit(nick=command[2])
-            print(member.display_name)
-        # await message.channel.send(response)
+            await member.add_roles(role)
+            await member.edit(nick=nick)
+
+            response = f"Welcome to RWTHufflepuff, {member.mention}. You have now been registered as {role.mention} Student.\nHave fun."
+        await message.channel.send(response)
 
     except Exception as e:
         await message.channel.send(f"hey {devRole.mention} There was an error.\n```\n{e}\n```")
