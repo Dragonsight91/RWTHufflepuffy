c85b984ef186852c31a1798958a64e328c76fea3
Dragonsight91 <r.vanemelen@icloud.com>
2020-02-17 00:30:04 +0100

    removed quote prototype

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -3,6 +3,5 @@ from . import voting
 from . import help
 from . import welcome
 from . import role
-from . import quote
 
-__all__ = [ "feat_req", "voting", "help", "quote" ]
+__all__ = [ "feat_req", "voting", "help" ]
