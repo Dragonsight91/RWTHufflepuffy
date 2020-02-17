a3aeb5b9052890993c818432d1f93639303ebbe8
Dragonsight91 <r.vanemelen@icloud.com>
2020-02-17 01:52:38 +0100

    moved a check to be executed first

--- src/commands/welcome.py
+++ src/commands/welcome.py
@@ -14,6 +14,10 @@ async def welcome_handler(bot, message):
         if len(message.role_mentions) == 0 or len(command) < 3:
             response = "**:x: WELCOME - MISSING PARAMETERS**\nOops, something went wrong. Please @mention the Role AND your Name.\nThe command can be used like this: `$welcome {role} {name}`"
         
+        # NO YOU DON'T
+        elif admin in message.role_mentions or devRole in message.role_mentions:
+            response = f"**:x: WELCOME - NOT PERMITTED**\nYou cannot give yourself the {admin.mention} or the {devRole.mention} Roles."
+
         # do you have that role??
         elif message.role_mentions[0] in member.roles:
             response = "**:no_entry: WELCOME - ROLE EXISTS**\nSorry, but you already have that role. If you wanted to change your role, you can use the `$role` command."
@@ -21,8 +25,7 @@ async def welcome_handler(bot, message):
         # why u try again? 
         elif len(member.roles) >1 and not (admin in member.roles or devRole in member.roles):
             response = "**:x: WELCOME - ALREADY REGISTERED**\nSorry, but you have already been registered. If you wanna change your role, you can use the `$role` command."
-        elif admin in message.role_mentions or devRole in message.role_mentions:
-            response = f"**:x: WELCOME - NOT PERMITTED**\nYou cannot give yourself the {admin.mention} or the {devRole.mention} Roles."
+
         # apparently all is good
         else:
             # get role and new Nick
