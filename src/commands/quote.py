97216f637ea673a969c3b4af6e35e08ca2f7758b
Dragonsight91 <r.vanemelen@icloud.com>
2020-02-17 21:34:31 +0100

    fixed missing quote module

---
+++ src/commands/quote.py
@@ -0,0 +1,31 @@
+import discord
+
+async def quote_handler(bot, message):
+    command = list(filter(lambda x : x != "" ,str(message.content).split(" ")))
+    devRole = discord.utils.get(message.author.guild.roles, name="Developer")
+
+    # :well at leas
+    try:
+        # USELESS INSPIRATION
+        if command[1] == "inspirobot":
+            response = await bot.inspirobot.get_quote()
+
+        # Actual Quotes
+        elif command[1] == "quotable":
+            response = await bot.quotable.get_quote()
+
+        # NOPE
+        else:
+            response = f"**:x: QUOTE - ACTION NOT FOUND**\nThat is not a supported API, please try one of the supported APIs. You can find out which APIs are supported by typing `$help quote`"
+
+        # welp... somethin ain't right
+        if not response:
+            await message.channel.send(f"**:pencil: QUOTE - {command[1].upper()} - ERROR**\nSorry, i couldn't get a response from `{command[1]}`, better luck next time.")
+        
+
+        if command[1] == "inspirobot":
+            await message.channel.send("**:pencil: QUOTE - INSPIROBOT**\n", {"files": response})
+        else:
+            await message.channel.send(f"**:pencil: QUOTE - {command[1].upper()}**\n```asciidoc\n===== QUOTE =====\n{response}\n```")
+    except Exception as e:
+        await message.channel.send(f"**:x: QUOTE - ERROR**\nHey {devRole.mention} There was an error.\n```\n{e}\n```")
